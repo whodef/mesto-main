c495c9b258cb484f7d5e76d2cf7ceb210529efba
Tatiana S <tatyana-selyuk@yandex.com>
2021-05-08 17:54:04 +0300

    Final fixes;

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 // Реализация блока с карточками
-let initialCards = [
+const initialCards = [
     {
         name: 'Архыз',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -26,10 +26,34 @@ let initialCards = [
     }
 ];
 
-const cardTemplate = document.querySelector('#card-template').content;
+const profileDescription = document.querySelector('.profile__description');
+const profileName = document.querySelector('.profile__name');
 const cardsContainer = document.querySelector('.card__list');
+const cardOverlayContainer = document.querySelector('#image-overlay');
+const cardOverlayImage = cardOverlayContainer.querySelector('.overlay__image');
+const cardOverlayCaption = cardOverlayContainer.querySelector('.overlay__image-caption');
+const overlayCloseButtons = document.querySelectorAll('.overlay__close-button');
 
-const getCards = (name, link) => {
+overlayCloseButtons.forEach((elem) => {
+    elem.addEventListener('click', (e) => {
+        e.preventDefault();
+        closeOverlay();
+    })
+});
+
+const openOverlay = (elem) => {
+    elem.classList.add('overlay_open');
+};
+
+const closeOverlay = () => {
+    document.querySelector('.overlay_open').classList.remove('overlay_open');
+};
+
+const overlayFormNameInput = () => document.querySelector('.overlay.overlay_open .overlay__form-input_type_name');
+const overlayFormExtInput = () => document.querySelector('.overlay.overlay_open .overlay__form-input_type_ext');
+
+const constructCard = (name, link) => {
+    const cardTemplate = document.querySelector('#card-template').content;
     const card = cardTemplate.querySelector('.card__item').cloneNode(true);
     const cardImage = card.querySelector('.card__image');
     const likeBtn = card.querySelector('.card__like-button');
@@ -49,134 +73,70 @@ const getCards = (name, link) => {
     });
 
     // Реализация всплывающего контейнера с описанием картинок
-    const cardOverlayContainer = document.querySelector('#image-overlay');
-    const cardOverlayImage = cardOverlayContainer.querySelector('.overlay__image');
-    const cardOverlayCaption = cardOverlayContainer.querySelector('.overlay__image-caption');
-    const cardOverlayCloseBtn = cardOverlayContainer.querySelector('.overlay__close-button');
-
-    const openOverlay = () => {
-        cardOverlayContainer.classList.add('overlay_open');
-    };
-
-    const closeOverlay = () => {
-        cardOverlayContainer.classList.remove('overlay_open');
-    };
 
     cardImage.addEventListener('click', (e) => {
         e.preventDefault();
         cardOverlayImage.src = link;
         cardOverlayImage.alt = name;
-        cardOverlayCaption.textContent = cardOverlayImage.alt;
-        openOverlay(cardOverlayContainer);
-    });
-
-    cardOverlayCloseBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        closeOverlay(cardOverlayContainer);
+        cardOverlayCaption.textContent = name;
+        cardOverlayContainer.classList.add('overlay_open');
     });
 
     return card;
 };
 
 initialCards.forEach((card) => {
-    cardsContainer.append(getCards(card.name, card.link));
+    cardsContainer.append(constructCard(card.name, card.link));
 });
 
 // Всплывающее окно для изменений в .profile__change-button
 const handleOverlayProfile = () => {
-
+    // Переменная названа контекстно
     const overlay = document.querySelector('#change-profile-overlay');
-
-    // profile block elements
-    // --------------
-    let profileDescription = document.querySelector('.profile__description');
-    let profileName = document.querySelector('.profile__name');
-
-    const openOverlay = () => {
-        overlay.classList.add('overlay_open');
-        nameInput.value = profileName.textContent;
-        descriptionInput.value = profileDescription.textContent;
-    };
-
-    const openOverlayButton = document.querySelector('#change-profile');
-    openOverlayButton.addEventListener('click', openOverlay);
-    // --------------
-
-    // overlay elements
-    // --------------
-    let nameInput = overlay.querySelector('.overlay__form-input_type_name');
-    let descriptionInput = overlay.querySelector('.overlay__form-input_type_description');
 
     const formSubmitHandler = (e) => {
         e.preventDefault();
-        profileName.textContent = nameInput.value;
-        profileDescription.textContent = descriptionInput.value;
+        profileName.textContent = overlayFormNameInput().value;
+        profileDescription.textContent = overlayFormExtInput().value;
         closeOverlay();
     };
 
-    const formElement = overlay.querySelector('.overlay__form');
-    formElement.addEventListener('submit', formSubmitHandler);
+    const openOverlayButton = document.querySelector('#change-profile');
 
-    const closeOverlay = () => {
-        overlay.classList.remove('overlay_open');
-    };
-
-    const closeOverlayButton = overlay.querySelector('.overlay__close-button');
-    closeOverlayButton.addEventListener('click', closeOverlay);
-    // --------------
+    openOverlayButton.addEventListener('click', () => {
+        openOverlay(overlay);
+        overlayFormNameInput().value = profileName.textContent;
+        overlayFormExtInput().value = profileDescription.textContent;
+        overlay.querySelector('.overlay__form').addEventListener('submit', formSubmitHandler);
+    });
 
 };
 
 // Всплывающее окно для изменений в .profile__add-button
-const handleOverlayNewCard = (initialCards, cards) => {
-
+const handleOverlayNewCard = () => {
+    // Переменная названа контекстно
     const overlay = document.querySelector('#new-card-overlay');
-
-    // profile block elements
-    // --------------
-    const openOverlay = () => {
-        overlay.classList.add('overlay_open');
-    };
-
-    const openOverlayButton = document.querySelector('#add-new-card');
-    openOverlayButton.addEventListener('click', openOverlay);
-    // --------------
-
-    // overlay elements
-    // --------------
-    let cardName = overlay.querySelector('.overlay__form-input_type_name');
-    let cardImageUrl = overlay.querySelector('.overlay__form-input_type_imageurl');
 
     const formSubmitHandler = (e) => {
         e.preventDefault();
+        const name = overlayFormNameInput();
+        const url = overlayFormExtInput();
 
-        initialCards.unshift({
-            name: cardName.value,
-            link: cardImageUrl.value
-        });
-
-        cardsContainer.prepend(cards(initialCards[0].name, initialCards[0].link));
-
+        cardsContainer.prepend(constructCard(name.value, url.value));
         closeOverlay();
 
-        cardName.value = '';
-        cardImageUrl.value = '';
+        name.value = '';
+        url.value = '';
     };
 
-    const formElement = overlay.querySelector('.overlay__form');
-    formElement.addEventListener('submit', formSubmitHandler);
+    const openOverlayButton = document.querySelector('#add-new-card');
 
-    const closeOverlay = () => {
-        overlay.classList.remove('overlay_open');
-    };
+    openOverlayButton.addEventListener('click', () => {
+        openOverlay(overlay);
+        overlay.querySelector('.overlay__form').addEventListener('submit', formSubmitHandler);
+    });
 
-    const closeOverlayButton = overlay.querySelector('.overlay__close-button');
-    closeOverlayButton.addEventListener('click', closeOverlay);
-    // --------------
 };
 
 handleOverlayProfile();
-handleOverlayNewCard(initialCards, getCards);
-
-
-
+handleOverlayNewCard();
